3a85ebdb2d66140d019a823a364d0506582291ae
xxfast <isurukusumal36@gmail.com>
2023-05-03 13:33:31 +1000

    Remove kotlin jvm target option

--- decompose-router-wear/build.gradle.kts
+++ decompose-router-wear/build.gradle.kts
@@ -24,10 +24,6 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
-
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
 }
 
 dependencies {
