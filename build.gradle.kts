5201416ad5f8d2ed6f07b80b0071e2285f9c977c
xxfast <isurukusumal36@gmail.com>
2023-05-03 13:59:22 +1000

    Add publishing and signing tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
-plugins { }
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+  id("org.jetbrains.dokka") version "1.8.10"
+}
 
 buildscript {
   repositories {
@@ -20,5 +24,82 @@ allprojects {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+  }
+
+  group = "io.github.xxfast"
+  version = "0.1.0-SNAPSHOT"
+
+  apply(plugin = "org.jetbrains.dokka")
+  apply(plugin = "maven-publish")
+  apply(plugin = "signing")
+
+  extensions.configure<PublishingExtension> {
+    repositories {
+      maven {
+        val isSnapshot = version.toString().endsWith("SNAPSHOT")
+        url = uri(
+          if (!isSnapshot) "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2"
+          else "https://s01.oss.sonatype.org/content/repositories/snapshots"
+        )
+
+        credentials {
+          username = gradleLocalProperties(rootDir).getProperty("sonatypeUsername")
+          password = gradleLocalProperties(rootDir).getProperty("sonatypePassword")
+        }
+      }
+    }
+
+    val javadocJar = tasks.register<Jar>("javadocJar") {
+      dependsOn(tasks.dokkaHtml)
+      archiveClassifier.set("javadoc")
+      from("$buildDir/dokka")
+    }
+
+    publications {
+      withType<MavenPublication> {
+        artifact(javadocJar)
+
+        pom {
+          name.set("Decompose-Router")
+          description.set("A Compose-multiplatform navigation library that leverage Decompose to create an API inspired by Conductor")
+          licenses {
+            license {
+              name.set("Apache-2.0")
+              url.set("https://opensource.org/licenses/Apache-2.0")
+            }
+          }
+          url.set("https://github.com/xxfast/Decompose-Router")
+          issueManagement {
+            system.set("Github")
+            url.set("https://github.com/xxfast/Decompose-Router/issues")
+          }
+          scm {
+            connection.set("https://github.com/xxfast/Decompose-Router.git")
+            url.set("https://github.com/xxfast/Decompose-Router")
+          }
+          developers {
+            developer {
+              name.set("Isuru Rajapakse")
+              email.set("isurukusumal36@gmail.com")
+            }
+          }
+        }
+      }
+    }
+  }
+
+  val publishing = extensions.getByType<PublishingExtension>()
+  extensions.configure<SigningExtension> {
+    useInMemoryPgpKeys(
+      gradleLocalProperties(rootDir).getProperty("gpgKeySecret"),
+      gradleLocalProperties(rootDir).getProperty("gpgKeyPassword"),
+    )
+
+    sign(publishing.publications)
+  }
+
+  // TODO: remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
+  project.tasks.withType(AbstractPublishToMaven::class.java).configureEach {
+    dependsOn(project.tasks.withType(Sign::class.java))
   }
 }
