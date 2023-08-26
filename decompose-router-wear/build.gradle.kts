9c3e4b1a00fbfc7b03916c8efb5135d5cc21361e
xxfast <isurukusumal36@gmail.com>
2023-08-26 14:07:34 +1000

    Bump all the dependencies

--- decompose-router-wear/build.gradle.kts
+++ decompose-router-wear/build.gradle.kts
@@ -41,9 +41,3 @@ android {
     targetCompatibility = JavaVersion.VERSION_17
   }
 }
-
-// TODO: Remove once a compiler with support for >1.8.21 available
-compose {
-  kotlinCompilerPlugin.set(dependencies.compiler.forKotlin("1.8.20"))
-  kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.8.22")
-}
