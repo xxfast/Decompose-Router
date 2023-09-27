b9eef78331ef5c93a5fe8da5ea3ac8924dc64353
xxfast <isurukusumal36@gmail.com>
2023-09-28 08:28:29 +1000

    Add default router context for ios targets

--- app/ios/ios/AppDelegate.swift
+++ app/ios/ios/AppDelegate.swift
@@ -5,12 +5,27 @@ import app
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
+  var rootRouterContext = RouterContextKt.defaultRouterContext()
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     window = UIWindow(frame: UIScreen.main.bounds)
-    let mainViewController = ApplicationKt.Main()
+    let mainViewController = ApplicationKt.MainUIController(routerContext: rootRouterContext)
     window?.rootViewController = mainViewController
     window?.makeKeyAndVisible()
     return true
+  }
+  
+  
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    RouterContextKt.resume(rootRouterContext.lifecycle)
+  }
+  
+  func applicationWillResignActive(_ application: UIApplication) {
+    RouterContextKt.stop(rootRouterContext.lifecycle)
+  }
+  
+  func applicationWillTerminate(_ application: UIApplication) {
+    RouterContextKt.destroy(rootRouterContext.lifecycle)
   }
 }
 
