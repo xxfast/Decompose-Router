6f72e289c1901793f06bd636c030096b3c09262f
xxfast <isurukusumal36@gmail.com>
2023-09-28 10:10:18 +1000

    Add default router context for js target

--- app/src/jsMain/kotlin/io/github/xxfast/decompose/router/app/Application.kt
+++ app/src/jsMain/kotlin/io/github/xxfast/decompose/router/app/Application.kt
@@ -2,16 +2,15 @@ package io.github.xxfast.decompose.router.app
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
-import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import io.github.xxfast.decompose.router.LocalRouterContext
 import io.github.xxfast.decompose.router.RouterContext
 import io.github.xxfast.decompose.router.app.screens.HomeScreen
 import io.github.xxfast.decompose.router.app.utils.BrowserViewportWindow
+import io.github.xxfast.decompose.router.defaultRouterContext
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
-  val lifecycle = LifecycleRegistry()
-  val rootRouterContext = RouterContext(lifecycle = lifecycle)
+  val rootRouterContext: RouterContext = defaultRouterContext()
 
   onWasmReady {
     BrowserViewportWindow("App") {
