c624dbf3e99ab62a15fdaade1970bc1c009f2112
xxfast <isurukusumal36@gmail.com>
2023-09-28 10:10:18 +1000

    Add default router context for js target

---
+++ decompose-router/src/jsMain/kotlin/io/github/xxfast/decompose/router/RouterContext.kt
@@ -0,0 +1,26 @@
+package io.github.xxfast.decompose.router
+
+import com.arkivanov.essenty.backhandler.BackDispatcher
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.lifecycle.resume
+import com.arkivanov.essenty.lifecycle.stop
+import kotlinx.browser.document
+import org.w3c.dom.Document
+
+fun defaultRouterContext(): RouterContext {
+  val backDispatcher = BackDispatcher()
+  val lifecycle = LifecycleRegistry()
+  lifecycle.attachToDocument()
+  return RouterContext(lifecycle = lifecycle, backHandler = backDispatcher)
+}
+
+// Attaches the LifecycleRegistry to the document
+private fun LifecycleRegistry.attachToDocument() {
+  fun onVisibilityChanged() = if (document.visibilityState == "visible") resume() else stop()
+  onVisibilityChanged()
+  document.addEventListener(type = "visibilitychange", callback = { onVisibilityChanged() })
+}
+
+private val Document.visibilityState: String
+  get() = asDynamic().visibilityState.unsafeCast<String>()
+
