8748e06a3a73f7f28dabcb6d376301337650a855
xxfast <isurukusumal36@gmail.com>
2024-05-26 12:47:09 +1000

    Fix compose js target

--- app/src/jsMain/kotlin/io/github/xxfast/decompose/router/app/Application.kt
+++ app/src/jsMain/kotlin/io/github/xxfast/decompose/router/app/Application.kt
@@ -2,18 +2,20 @@ package io.github.xxfast.decompose.router.app
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
 import io.github.xxfast.decompose.router.LocalRouterContext
 import io.github.xxfast.decompose.router.RouterContext
 import io.github.xxfast.decompose.router.screens.HomeScreen
-import io.github.xxfast.decompose.router.app.utils.BrowserViewportWindow
 import io.github.xxfast.decompose.router.defaultRouterContext
 import org.jetbrains.skiko.wasm.onWasmReady
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
   val rootRouterContext: RouterContext = defaultRouterContext()
 
   onWasmReady {
-    BrowserViewportWindow("App") {
+    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
       CompositionLocalProvider(
         LocalRouterContext provides rootRouterContext,
       ) {
